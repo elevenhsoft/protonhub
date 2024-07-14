9d346da808a34b2939328b297dc14485883af3bc
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-07-14 14:40:19 +0200

    serve static files

--- routing.go
+++ routing.go
@@ -6,4 +6,6 @@ func Routing(mux *http.ServeMux) {
 	mux.HandleFunc("GET /", RootHandler)
 	mux.HandleFunc("GET /create", CreateHandler)
 	mux.HandleFunc("POST /create", CreateDoneHandler)
+
+	mux.Handle("GET /assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./static/assets/"))))
 }
